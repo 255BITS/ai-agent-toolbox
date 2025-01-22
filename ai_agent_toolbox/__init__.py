da77941a34df7e3e9c4f9e8b270bae33727cb572
Martyn Garcia <martyn.garcia@gmail.com>
2025-01-21 21:17:25 -0700

    fix imports

--- ai_agent_toolbox/__init__.py
+++ ai_agent_toolbox/__init__.py
@@ -1,8 +1,9 @@
 from .toolbox import Toolbox
 from .parsers.xml.xml_parser import XMLParser
 from .parsers.xml.flat_xml_parser import FlatXMLParser
+from .formatters.xml.xml_prompt_formatter import XMLPromptFormatter
 from .parser_event import ParserEvent
 
 __all__ = [
-    "Toolbox", "Tool", "XMLParser", "FlatXMLParser", "ParserEvent"
+    "Toolbox", "Tool", "XMLParser", "FlatXMLParser", "ParserEvent", "XMLPromptFormatter"
 ]
