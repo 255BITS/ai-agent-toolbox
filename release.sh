c324286bb360041c29a9bcab657cf302fc7f5bb2
Martyn Garcia <martyn.garcia@gmail.com>
2025-01-26 21:50:35 -0700

    add release script

---
+++ release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+# Check arguments
+if [ $# -ne 1 ]; then
+  echo "Usage: ./release.sh <version-part>"
+  echo "Example: ./release.sh minor"
+  exit 1
+fi
+
+# Bump version (patch, minor, major)
+bump2version $1
+
+# Push changes and tags to GitHub
+git push origin main --tags
+
+# Build and upload to PyPI
+python -m build
+twine upload dist/*
