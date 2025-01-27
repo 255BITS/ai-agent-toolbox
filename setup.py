efc331846d5fce037f2182e61b0cda383ab4f171
Martyn Garcia <martyn.garcia@gmail.com>
2025-01-26 21:54:15 -0700

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ai_agent_toolbox",
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],
