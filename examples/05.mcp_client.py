a47d86b6e0542ef45a1081bccdbe525e1d94e3d0
Martyn Garcia <martyn.garcia@gmail.com>
2025-01-31 20:32:26 -0700

    wip mcp

--- examples/05.mcp_client.py
+++ examples/05.mcp_client.py
@@ -14,6 +14,18 @@ server_params = StdioServerParameters(
     args=[str(Path(__file__).parent.resolve() / "05.mcp_server.py")],
 )
 
+def convert_input_schema(inputSchema):
+    args = {}
+    properties = inputSchema["properties"]
+    for key in properties.keys():
+        args[key]={"type": properties[key]["type"], "description": properties[key]['title']}
+    return args
+
+async def call_tool(tool):
+    async def _call_tool(*args, **kwargs):
+        pass
+    return _call_tool
+
 async def run():
     async with stdio_client(server_params) as (read, write):
         async with ClientSession(read, write) as session:
@@ -21,8 +33,12 @@ async def run():
             await session.initialize()
             print("Listing tools")
             tools = await session.list_tools()
-            for tool in tools:
-                print("tool is", tool)
+            for tool_tuple in tools:
+                if tool_tuple[0] == "tools":
+                    for tool in tool_tuple[1]:
+                        print("tool is", tool)
+                        toolbox.add_tool(name=tool.name, description=tool.description, **convert_input_schema(tool.inputSchema))
+                        print("--", toolbox._tools)
                 #toolbox.add_mcp_tool(tool)
 
             #response = anthropic_llm_call(system_prompt=system, prompt=prompt)
